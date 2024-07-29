e3e56c62092a211e97a9125dae13284478e15e85
Narayani22 <119776291+Narayani22@users.noreply.github.com>
2024-07-29 16:31:02 +0530

    Add files via upload

---
+++ program287.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Pattern
+{
+    public void Display()
+    {
+
+    }
+}
+
+class program287
+{
+    public static void main(String a[])
+    {
+        Pattern pobj = new Pattern();
+        Scanner sobj = new Scanner(System.in);
+
+        pobj.Display();
+    }
+}
